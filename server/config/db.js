b14a7fad750870ef7f17e3107a0456e10f50f248
Sai Charan Thota <163974930+CharanSai8127@users.noreply.github.com>
2025-02-06 16:37:09 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 
 // Use environment variables to configure the database connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost', // Default to 'mysql' if DB_HOST is not set
+  host: process.env.DB_HOST || 'mysql', // Default to 'mysql' if DB_HOST is not set
   user: process.env.DB_USER || 'root',  // Default to 'root' if DB_USER is not set
   password: process.env.DB_PASSWORD || 'password',  // Default to 'password' if DB_PASSWORD is not set
   database: process.env.DB_NAME || 'test_db',  // Default to 'test_db' if DB_NAME is not set
